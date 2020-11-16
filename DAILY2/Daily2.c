27e048a9d991974034d8e0c12bd7568a18a13905
Brett Beaulieu <74109542+bdbeau21@users.noreply.github.com>
2020-11-15 19:03:22 -0500

    Create Daily2.c

---
+++ DAILY2/Daily2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // Outputs name and something about me
+    printf("Hello, my name is ....... \nThis class is Awesome Sauce! \n"
+           "Something about me: I play club tennis for Umass Lowell \n");
+    return 0;
+}
