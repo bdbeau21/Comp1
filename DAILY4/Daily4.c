09e43dcb5b669b95e6b823e59505e7a79f3824ad
Brett Beaulieu <74109542+bdbeau21@users.noreply.github.com>
2020-11-15 19:17:46 -0500

    Create Daily4.c

---
+++ DAILY4/Daily4.c
@@ -0,0 +1,24 @@
+// PREPROCESSOR DIRECTOR
+#include <stdio.h>
+
+// MAIN FUNCTION
+int main(int argc, const char * argv[]) {
+    
+    smile();
+    printf("\n");
+    smile();
+    printf("\n");
+    smile();
+    return 0;
+}
+
+/* FUNCTION PRINTS OUT A SMILE FACE */
+int smile(void) {
+    printf("  ****  \n");
+    printf(" * . .*  \n");
+    printf("*      * \n");
+    printf("*  \\_/ * \n");
+    printf(" *    *  \n");
+    printf("  ****  \n");
+    return 0;
+}
